62c6761ca1e33be284f9e63dcfe45828944537d6
Sanaqamar123 <sana.qamr@gmail.com>
2020-09-23 12:34:34 +0530

    New some more changes

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,4 @@
 #!/bin/bash -x
 echo "welcome"
 num=$((RANDOM%2))
-if [ $num == 1 ]
-then
-	echo "Employee is present"
-else
-	echo "Employee is absent"
-fi
-echo "new  some change"
+echo "new  some more change"
